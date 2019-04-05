5839f55e19f9360f4c79184ed52923a547f63c86
vine <>
2019-04-06 02:46:01 +0900

    first commit

---
+++ src/lernCA/Main.java
@@ -0,0 +1,23 @@
+package learnCA;
+
+import processing.core.PApplet;
+
+public class Main extends PApplet {
+    public void settings() {
+        size(640,360);
+    }
+
+
+    public void setup() {
+
+    }
+
+
+    public void draw() {
+
+    }
+
+    public static void main(String[] args){
+        PApplet.main("learnCA.Main");
+    }
+}
